c665c8e8d8922efc754acc8bd25a0fb7a2ee4292
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-16 08:52:59 +0800

    Arrays Left Rotation Problem & Passing Solution

--- InterviewPrep/Arrays/ArraysLeftRotation.js
+++ InterviewPrep/Arrays/ArraysLeftRotation.js
@@ -12,6 +12,15 @@
 * Given an array a of n integers and a number, d, perform d left rotations on the array.
 * Return the updated array to be printed as a single line of space-separated integers.
 *
+* --------------------
+* Function Description
+* --------------------
+* Complete the function rotLeft in the editor below.
+* 
+* rotLeft has the following parameter(s):
+* 
+* int a[n]: the array to rotate
+* int d: the number of rotations
 */
 
 // Input
@@ -21,6 +30,19 @@ const d = 4;
 function rotLeft(a, d) {
     // Write your code here
 
+    /* 
+    * 1. In constraints, note 1 <= d <= n, meaning the number of rotations is less than or equal to the # elements in array a
+    * 2. array a will be at least left rotated by 1 element since d is greater than or equal to 1
+    * 3. following the logic of point 1. above, the array a cannot be left rotated more than n so we can use splice(0, d)
+    * 4. add the elements of the splice array to the tail of the array a using push / spread operator
+    */ 
+//    let splicedArrayToRotate = a.splice(0,d);
+//    a.push(...splicedArrayToRotate);
+//    // return the rotated array a
+//    return a;
+    const splicedArray = a.splice(0, d);
+    a.push(...splicedArray);
+    return a;
 };
 
-rotLeft(a,d)
+console.log(rotLeft(a,d))
