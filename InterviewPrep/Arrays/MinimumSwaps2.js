6ea441aad1736dee95c54ce248020bed9ecfa332
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-17 11:35:47 +0800

    add Arrays - Minimum Swaps 2

---
+++ InterviewPrep/Arrays/MinimumSwaps2.js
@@ -0,0 +1,53 @@
+/**
+* Link: https://www.hackerrank.com/challenges/minimum-swaps-2/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
+* Name: Arrays - Minimum Swaps 2
+*
+* -----------
+* Description
+* -----------
+* You are given an unordered array consisting of consecutive integers  [1, 2, 3, ..., n] without any duplicates.
+* You are allowed to swap any two elements.
+* Find the minimum number of swaps required to sort the array in ascending order.
+*
+* -------
+* Example
+* -------
+* arr = [7,1,3,2,4,5,6]
+* i   arr                     swap (indices)
+* 0   [7, 1, 3, 2, 4, 5, 6]   swap (0,3)
+* 1   [2, 1, 3, 7, 4, 5, 6]   swap (0,1)
+* 2   [1, 2, 3, 7, 4, 5, 6]   swap (3,4)
+* 3   [1, 2, 3, 4, 7, 5, 6]   swap (4,5)
+* 4   [1, 2, 3, 4, 5, 7, 6]   swap (5,6)
+* 5   [1, 2, 3, 4, 5, 6, 7]
+* It took 5 swaps to sort the array.
+*
+* --------------------
+* Function Description
+* --------------------
+* Complete the function minimumSwaps in the editor below.
+*
+* minimumSwaps has the following parameter(s):
+*
+* int arr[n]: an unordered array of integers
+*
+* -------
+* Returns
+* -------
+* int: the minimum number of swaps to sort the array
+*
+* -----------
+* Constraints
+* -----------
+* 1 <= n <= 10^5
+* 1 <= arr[i] <= n
+*/
+
+// Input
+const arr = [7,1,3,2,4,5,6];
+
+function minimumSwaps(arr) {
+
+
+}
+
