d36d099c5408384326b10a8ee7993ddbfd1733cf
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-19 12:11:07 +0800

    Queues A Tale Of Two Stacks Problem Setup

---
+++ InterviewPrep/Stacks and Queues/QueuesATaleofTwoStacks.js
@@ -0,0 +1,22 @@
+/**
+ * Link: https://www.hackerrank.com/challenges/ctci-queue-using-two-stacks/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues
+ * Name: Queues: A Tale of Two Stacks
+ * Difficulty: Medium
+ *
+ */
+
+// Input: The first line contains a single integer, q, the number of queries.
+const q = [[1, 42], [2,''], [1, 14], [3,''], [1, 28], [3,''], [1, 60], [1, 78], [2,''], [2,'']];
+// Steps:
+// 1. if 1, then enqueue (push)
+// 2. if 2, then dequeue (shift)
+// 3. if 3, then print from first in queue/stack (FIFO) (peek)
+
+// For each query of type 3, return the value of the element at the front of the fifo queue on a new line.
+
+
+function processData(input) {
+    //Enter your code here
+};
+
+console.log(processData(input))
