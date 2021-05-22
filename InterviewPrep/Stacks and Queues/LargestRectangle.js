ca623ca9f820e736db2805fa214bf8f04cb5bd6f
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-22 08:06:31 +0800

    Largest Rectangle Problem & Setup: Medium Diffi

---
+++ InterviewPrep/Stacks and Queues/LargestRectangle.js
@@ -0,0 +1,26 @@
+/**
+ * Link: https://www.hackerrank.com/challenges/largest-rectangle/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues
+ * Name: Largest Rectangle
+ * Difficulty: Medium
+ *
+ *
+ * Objective: Find the largest solid area which is a rectangle.
+ * -------
+ * Returns
+ * -------
+ * int: largestArea
+*/
+
+// Input:
+
+// Example 1 : Expected Output 6
+// const height = [3,2,3];
+//Example 2 : Expect Output 9
+const height = [1,2,3,4,5];
+
+function largestRectangle(height) {
+    // Write your code here
+
+};
+
+console.log(largestRectangle(height));
