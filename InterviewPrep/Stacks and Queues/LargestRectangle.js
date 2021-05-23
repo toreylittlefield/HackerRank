fe95237a180040d2a0be543778f086cad1de36f6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-23 10:57:27 +0800

    Largest Rectangle (Histogram) Prob + Passing Tests

--- InterviewPrep/Stacks and Queues/LargestRectangle.js
+++ InterviewPrep/Stacks and Queues/LargestRectangle.js
@@ -22,12 +22,17 @@
 // Example 3: Expected 50
 // const height = [11,11,10,10,10];
 
-// Example 4: expect 20
-// const height = [1, 4, 3, 5, 6, 2, 4, 5, 5, 5, 5, 30, 2, 1, 2];
+// Example 4: expect 18
+const height = [1, 2, 3, 5, 6, 2, 4, 5, 5, 5, 1, 15, 8, 1, 2];
+
+// Example 5: expected 26152
+// const height = [8979, 4570, 6436, 5083, 7780, 3269, 5400, 7579, 2324, 2116]
 
 // Example 5: expect 18
-const height = [1, 3, 5, 9, 11]
+// const height = [1, 3, 5, 9, 11]
 
+
+// Some thoughts on the problem...
 // potential largest areas = [1 * length of array, (minHeight > 1 * anotherArray(minHeight > 1 start, minHeight > 1 end)
 // ]
 // lengthArea = 12 * 1 = 12 --> height.length * 1
@@ -35,83 +40,71 @@ const height = [1, 3, 5, 9, 11]
 // areaWithMin = (starts at 0 ends at index 11 minHeight(2) * 12 = 24 --> minHeight > 1; startIndex
 // areaWithOtherMin = starts at index 5 ends at index 12 this minHeight is 4, so 4 * 7 = 28
 // return largest Area which is the 30 in this case (largestAreaByInt)
-console.log(height.length)
-// areas to compare
 
 
-function largestRectangle(height) {
-    // Write your code here
-    let lengthArea = height.length;
-    let largestIntArea = Math.max(...height);
-    let firstMin = height[0];
-    let firstMinArray = [];
+function largestRectangle(heightsArray) {
+    // two stacks track heights and index along the array
+    let posIndexStack = [];
+    let heightStack = [];
 
+    // area to compare & return
     let maxArea = 1;
-    // console.log(lengthArea, largestIntArea)
 
-    let areasStartToEnd = [lengthArea, largestIntArea]
+    // track the starting index position in the posIndexStack for the current rectangle
+    let startingIndex = 0;
+    
+    let heightArrayLength = heightsArray.length;
 
-    function findAreas (height) {
-        for (let index = 0; index < height.length; index++) {
-            const curValue = height[index];
-    
-            // firstMin must be greater than 1
-            if (firstMin === 1) {
-                firstMin = curValue;
-            };
-    
-            // swap firstMin with lowest currentValue
-            if (curValue < firstMin && curValue > 1) {
-                firstMin = curValue;
-            };
-    
-            // keep adding to length of array
-            if (curValue >= firstMin && curValue > 1) {
-                firstMinArray.push(curValue);
-            };
 
-            // if 1 && 2nd item or at the end of the array or current value > first
-            if (curValue === 1 && index > 0 || index === height.length - 1 || curValue > firstMin) {
-                areasStartToEnd.push(firstMinArray.length * firstMin);
-            };
-            // console.log(firstMinArray)
-            // console.log(firstMin)
-        };
-        // findAreas(firstMinArray);
-    };
-    findAreas(height);
 
-    function findRemainingArea () {
-        if(firstMinArray.length === 0) return;
-        let secondMin = firstMinArray[1];
-        let secondMinArray = [];
-        for (let j = 1; j < firstMinArray.length; j++) {
-            const curV = firstMinArray[j];
-            // swap firstMin with lowest currentValue
-            if (curV < secondMin && curV > 1) {
-                secondMin = curV;
-            };
-            // keep adding to length of array
-            if (curV >= secondMin && curV > 1) {
-                secondMinArray.push(curV);
-            };
-    
-            if(j === firstMinArray.length - 2|| curV > secondMin) {
-                // console.log(secondMinArray, secondMin)
-                areasStartToEnd.push(secondMinArray.length * secondMin);
+    findAreas(heightsArray)
+
+    // loop over areas & push to stacks
+    function findAreas(heightsArray) {
+        for (let index = 0; index < heightArrayLength; index++) {
+            const currentHeight = heightsArray[index];
+            // push height & index onto stacks if current height is greater than last height in stack
+            if(currentHeight > peekStack(heightStack)) {
+                posIndexStack.push(index);
+                heightStack.push(currentHeight);
             }
+            // if currentHeight is less than last height pop & calc area then add back index from top of stack & currentHeight
+            else if(currentHeight < peekStack(heightStack)) {
+                while(heightStack.length > 0 && currentHeight < peekStack(heightStack)){
+                    compareMaxArea(index);
+                };
+                posIndexStack.push(startingIndex);
+                heightStack.push(currentHeight);
+            }
+            // if last element loop over the stacks and compare areas until stacks are empty
+            if (index === heightArrayLength - 1) {
+                // loop over stack until empty
+                while(heightStack.length > 0) {
+                    compareMaxArea(index + 1);
+                }
+            };
         };
-        firstMinArray.shift();
-        findRemainingArea()
-    }
-    findRemainingArea()
-
-
-    // console.log(areasStartToEnd)
-    // console.log(maxArea)
-    if(areasStartToEnd.length > 1) {
-        maxArea = Math.max(...areasStartToEnd)
     };
+
+    // peek a stack
+    function peekStack(array) {
+        if(array.length === 0) return 0;
+        return array[array.length - 1];
+    };
+
+    // compare currentArea To Largest Area Found
+    function compareMaxArea(currentIndex) {
+        let tempHeight = heightStack.pop();
+        let tempIndex = posIndexStack.pop();
+        let tempWidth = currentIndex - tempIndex;
+        let tempArea = tempHeight * tempWidth;
+        maxArea = Math.max(tempArea, maxArea);
+
+        // return the last tempIndex back to the posIndexStack stack
+        startingIndex >= 0 ? startingIndex = tempIndex : null;
+    }
+
+    // return the maxArea largest rectangle
     return maxArea;
 };
 
