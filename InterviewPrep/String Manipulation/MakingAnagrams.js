840e3ba0fc9a458d4ef690c58a47efd14aa6d913
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-24 11:23:35 +0800

    String Manipulation: Making Anagrams Setup

---
+++ InterviewPrep/String Manipulation/MakingAnagrams.js
@@ -0,0 +1,19 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/ctci-making-anagrams/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+ * Type: String Manipulation
+ * Difficulty: Easy
+ * Name: Strings: Making Anagrams
+ */
+
+// Input
+const a = 'cde';
+const b = 'abc';
+
+// Contraints: The strings  and  consist of lowercase English alphabetic letters, ascii[a-z].
+
+function makeAnagram(a, b) {
+    // Write your code here
+
+}
+
+console.log(makeAnagram(a, b))
