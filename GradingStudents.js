207bf0fb15097f4e5eea87775816d9f476d41228
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-24 10:06:17 +0800

    Grading Students passing all tests

--- GradingStudents.js
+++ GradingStudents.js
@@ -10,7 +10,13 @@ const grades = [73, 67, 38, 33];
 
 function gradingStudents(grades) {
     // Write your code here
-
+    return grades.map((grade) => {
+        const nextMultOfFive = grade % 5;
+        if(nextMultOfFive > 2 && grade >= 38) {
+            return grade + 5 - nextMultOfFive;
+        }
+        return grade;
+    })
 }
 
 console.log(gradingStudents(grades));
