a2681d867912129298f267e7b2af29fb97e59ce1
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-24 09:34:35 +0800

    Grading Students Problem & Setup : Easy / Algo

---
+++ GradingStudents.js
@@ -0,0 +1,16 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/grading/problem
+ * Type: Algorithms / Implementation
+ * Difficulty: Easy
+ * Name: Grading Students
+ */
+
+// Input
+const grades = [73, 67, 38, 33];
+
+function gradingStudents(grades) {
+    // Write your code here
+
+}
+
+console.log(gradingStudents(grades));
