3877e3f91cbced8d2c70adf8c17e49185e8043ce
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-25 12:40:25 +0800

    fix bug rename function from solve to riddle

--- InterviewPrep/Stacks and Queues/MinMaxRiddle.js
+++ InterviewPrep/Stacks and Queues/MinMaxRiddle.js
@@ -11,13 +11,20 @@
 const arr = [3, 5, 4, 7, 6, 2];
 
 // Example 2: Expect 1 2 3 5 1 13 3
-const arr = [1, 2, 3, 5, 1, 13, 3];
+// const arr = [1, 2, 3, 5, 1, 13, 3];
 
 // Returns array: Single line containing n space-separated integers denoting the output for each window size from 1 to n.
 
-function solve(arr) {
+function riddle(arr) {
     // write code here
+    let maxWinsArray = [];
 
+    //first arr find max for window 1
+    //second: window 2 pair mins of (n[0], n[1]) , (n[1], n[2])... (n[n-2], n[n-1]) then find max amongst all possibile mins
+    // repeat...
+
+    // return an array with maximums
+    return maxWinsArray;
 }
 
 console.log(solve(arr));
