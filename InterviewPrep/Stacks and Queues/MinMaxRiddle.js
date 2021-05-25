9e24c04a40cd66c8cfbb33752cb23d261f1f36a7
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-25 12:03:16 +0800

    Min Max Riddle Stacks & Queues Setup

---
+++ InterviewPrep/Stacks and Queues/MinMaxRiddle.js
@@ -0,0 +1,23 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/min-max-riddle/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=stacks-queues
+ * Type: Stacks and Queues
+ * Difficulty: Medium
+ * Name: Min Max Riddle
+ */
+
+// Input
+
+// Example 1: Expect 7 6 4 4 3 2
+const arr = [3, 5, 4, 7, 6, 2];
+
+// Example 2: Expect 1 2 3 5 1 13 3
+const arr = [1, 2, 3, 5, 1, 13, 3];
+
+// Returns array: Single line containing n space-separated integers denoting the output for each window size from 1 to n.
+
+function solve(arr) {
+    // write code here
+
+}
+
+console.log(solve(arr));
