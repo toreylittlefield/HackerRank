58a18224b34707349c871d89339c51718eb772b0
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-28 05:39:41 +0800

    Bubble Sort problem setup: Sorting, Easy

---
+++ InterviewPrep/Sorting/BubbleSort.js
@@ -0,0 +1,25 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/ctci-bubble-sort/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+ * Type: Sorting
+ * Difficulty: Easy
+ * Name: Sorting: Bubble Sort
+ */
+
+// Returns & Prints
+// Print the three lines required, then return. No return value is expected.
+
+// Example 1:
+const a = [1,2,3]
+/**
+ * Expected Output:
+ * Array is sorted in 0 swaps.
+ * First Element: 1
+ * Last Element: 3
+ */
+
+function countSwaps(a) {
+    // Write your code here
+
+}
+
+console.log(countSwaps(a))
