c5ccbfdfade59cfea8893c81242443864bb34a8b
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-27 15:50:37 +0800

    Add Recursion Folder + Fibonacci Problem

---
+++ InterviewPrep/Recursion/FibonacciNumbers.js
@@ -0,0 +1,22 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=recursion-backtracking
+ * Type: Recursion and Backtracking
+ * Difficulty: Easy
+ * Name: Recursion: Fibonacci Numbers
+ */
+
+// Input
+
+// Example 1: Expect 5
+const n = 5
+
+// Example 2: Expect 144
+// const n = 12
+
+// Returns int the nth element in the Fibonacci sequence
+
+function fibonacci(n) {
+    
+}
+
+console.log(fibonacci(n));
