31264db3f981e67e8b20ca12a726e363866c14a9
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-28 06:33:58 +0800

    Mark And Toys passing all tests / solution

--- InterviewPrep/Sorting/MarkAndToys.js
+++ InterviewPrep/Sorting/MarkAndToys.js
@@ -5,23 +5,36 @@
  * Name: Mark And Toys
  */
 
-// Returns int: the maximum number of toys;
+// Returns int: the maximum number of toys *NOTE A toy can't be bought multiple times.;
 
 // Example 1: Expect 4
-const prices = [1, 12, 5, 111, 200, 1000, 10]
-const k = 50;
+// const prices = [1, 12, 5, 111, 200, 1000, 10]
+// const k = 50;
 
 // Example 2: Expect 3
-// const prices = [1,2,3,4]
-// const k = 7
+const prices = [1,2,3,4]
+const k = 7
 
 
- function maximumToys(prices, k) {
+function maximumToys(prices, k) {
     // Write your code here
-    let numToysBudget = 0;
+    let numToysWithinBudget = 0;
+
+    let n = prices.length;
+
+    let sum = 0;
+
+    prices.sort((a, b ) => a - b);
+
+    // find max toys by sorting
+    for (let index = 0; index < n; index++) {
+        if(sum + prices[index] >= k) break;
+        sum += prices[index];
+        numToysWithinBudget++;
+    };
 
     // return the num of toys available to purchase within the budget k and price list
-    return numToysBudget;
+    return numToysWithinBudget;
 }
 
 console.log(maximumToys(prices, k))
