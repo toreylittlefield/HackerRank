f2289895da5fe2f6fc76f460d6b0f16b4d297330
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-28 06:42:33 +0800

    Sorting Comparator problem setup: medium

---
+++ InterviewPrep/Sorting/SortingComparator.js
@@ -0,0 +1,34 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+ * Type: Sorting
+ * Difficulty: Medium
+ * Name: Sorting: Comparator
+ */
+
+/***
+ * Comparator.compare(T o1, T o2)
+ * Declare a Checker class that implements the comparator method as described.
+ * It should sort first descending by score, then ascending by name.
+ */
+
+// Example 1: 
+const data = [
+    'amy 100',
+    'david 100',
+    'heraldo 50',
+    'aakansha 75',
+    'aleksa 150'
+  ];
+/***
+ * Expected Output
+ * aleksa 150
+ * amy 100
+ * david 100
+ * aakansha 75
+ * heraldo 50
+ */
+
+function main(data) {
+}
+
+console.log(main(data));
