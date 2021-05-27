74fc4db6c85e48dcb5852c7d14a9ee11f84e16d6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-27 19:08:34 +0800

    PIcking Numbers: problem setup: easy

---
+++ Practice/Algorithms/Implementation/PickingNumbers.js
@@ -0,0 +1,13 @@
+/**
+ * Name: Picking Numbers
+ * Link: https://www.hackerrank.com/challenges/picking-numbers/problem
+ * Difficulty: Easy
+ */
+
+const a = [4, 6, 5, 3, 3, 1];
+
+function pickingNumbers(a) {
+
+};
+
+console.log(pickingNumbers(a));
