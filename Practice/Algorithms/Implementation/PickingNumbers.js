7a07fff99aa04205a5c941f13e4b7d094fe0e460
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-27 19:27:43 +0800

    Picking Numbers setup

--- Practice/Algorithms/Implementation/PickingNumbers.js
+++ Practice/Algorithms/Implementation/PickingNumbers.js
@@ -4,10 +4,17 @@
  * Difficulty: Easy
  */
 
+// Example 1: Expected {4, 3, 3} which is length 3
 const a = [4, 6, 5, 3, 3, 1];
 
-function pickingNumbers(a) {
+//find the longest subarray where the absolute difference between any two elements is less than or equal to 1.
 
+function pickingNumbers(a) {
+    let maxSub = 0;
+
+
+    // return max sub array
+    return maxSub;
 };
 
 console.log(pickingNumbers(a));
