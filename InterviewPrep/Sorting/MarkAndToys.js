14bcb64c8db1f77c25d065d2a3d6b7b84b220b3d
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-28 06:08:43 +0800

    Mark and Toys problem setup: Sorting / Easy

---
+++ InterviewPrep/Sorting/MarkAndToys.js
@@ -0,0 +1,27 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/mark-and-toys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+ * Type: Sorting
+ * Difficulty: Easy
+ * Name: Mark And Toys
+ */
+
+// Returns int: the maximum number of toys;
+
+// Example 1: Expect 4
+const prices = [1, 12, 5, 111, 200, 1000, 10]
+const k = 50;
+
+// Example 2: Expect 3
+// const prices = [1,2,3,4]
+// const k = 7
+
+
+ function maximumToys(prices, k) {
+    // Write your code here
+    let numToysBudget = 0;
+
+    // return the num of toys available to purchase within the budget k and price list
+    return numToysBudget;
+}
+
+console.log(maximumToys(prices, k))
