1b43f7c2466f41373c36619cada26348244964c3
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-28 09:20:29 +0800

    naive solution passes only 3 tests

--- InterviewPrep/Sorting/FraudulentActivityNotifications.js
+++ InterviewPrep/Sorting/FraudulentActivityNotifications.js
@@ -21,17 +21,70 @@
 // Returns int: the number of notices sent
 
 // Example 1: Expect 0
-const expenditure = [1, 2, 3, 4, 4]
+// const expenditure = [1, 2, 3, 4, 4]
 // const d = 4;
 
 // Example 2: Expect 2
+// sorted 2 2 3 3 4 4 5 6 8
 // const expenditure = [2, 3, 4, 2, 3, 6, 8, 4, 5]
-const d = 5
+// const d = 5
+
+// Exmaple 3: Expect 1
+const expenditure = [10,5,2,4,10,2]
+const d = 2
 
 
 function activityNotifications(expenditure, d) {
-    // Write your code here
+    /***
+     * Steps:
+     * 1. Create a window / span of length d and right
+     * 2. sort the window & while sorting find the median value
+     * 3. To find the median, the formula is window[d/2] if d is odd or (window[d/2] + window[d/2 + 1])/2 if even if the window contains the ints of spanning d length
+     * 4. Check if the next value is 2X the median value if true add 1 to the total of numAlertsSent;
+     * 5. Move the window from the left by 1 and right by 1, the new median calculated again after sorting
+     * 6. Check if next value is 2X... repeat until right pointer reaches the end of expenditures
+     */
 
+    const n = expenditure.length;
+
+    let numAlertsSent = 0;
+    let medianValue = 0;
+    let window = [];
+
+    for(let right = 0; right < n; right++) {
+        const curVal = expenditure[right];
+        if(window.length !== d) {
+            window.push(curVal);
+            continue;
+        }
+        const copyData = [...window];
+        medianValue = calcMedian(copyData);
+        
+        // check median against current value
+        if(curVal >= medianValue * 2) {
+            numAlertsSent++;
+        };
+
+        window.shift();
+        window.push(curVal);
+    }
+
+    // [2, 3, 4, 2, 3, 6, 8, 4, 5]
+
+    // return number of alerts sent
+    return numAlertsSent;
+
+    function calcMedian (data) {
+        const len = data.length;
+        data.sort((a, b) => a - b);
+        let even = len % 2 === 0;
+        if(even) {
+            const medianNumPos = Math.floor(data.length / 2);
+            return medianValue = (data[medianNumPos - 1] + data[medianNumPos]) / 2
+        };
+        const medianNumPos = Math.floor(data.length / 2) + 1;
+        return medianValue = data[medianNumPos - 1];
+    };
 }
 
 console.log(activityNotifications(expenditure, d))
