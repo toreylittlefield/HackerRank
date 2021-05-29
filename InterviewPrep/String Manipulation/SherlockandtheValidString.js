0ec056b8d97f379441238225d4dd27f621003a44
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-30 07:32:22 +0800

    Sherlock and the Valid String problem setup

---
+++ InterviewPrep/String Manipulation/SherlockandtheValidString.js
@@ -0,0 +1,24 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/sherlock-and-valid-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=strings
+ * Type: String Manipulation
+ * Difficulty: Medium
+ * Name: Sherlock and the Valid String
+ */
+
+// Input string s: a string
+
+// Outputstring: either YES or NO
+
+// Example 1: Expect 'YES'
+const s = `abcdefghhgfedecba`
+
+// Example 2: Expect 'NO'
+const s = `aabbccddeefghi`
+
+function isValid(s) {
+
+
+}
+
+console.log(isValid(s))
+
