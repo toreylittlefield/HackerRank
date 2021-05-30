7a137f7eac5816b7d6775193f9b61e48ba9c4818
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-30 11:11:43 +0800

    Max Array Sum passing all tests

--- InterviewPrep/Dynamic Programming/MaxArraySum.js
+++ InterviewPrep/Dynamic Programming/MaxArraySum.js
@@ -18,7 +18,7 @@
 // Output - int: the maximum subset sum
 
 // Example 1: Expect 13
-const arr = [3, 7, 4, 6, 5]
+// const arr = [3, 7, 4, 6, 5]
 
 // Example 2: Expect 11
 // const arr = [2, 1, 5, 8, 4]
@@ -26,18 +26,36 @@ const arr = [3, 7, 4, 6, 5]
 // Example 3: Expect 0 ** Special Negative Case
 // const arr = [-2,-3,-1];
 
+// Example 4: Expect 100
+const arr = [-1, -2, -3, -4, 5, 100, -7, -6]
+
 function maxSubsetSum(arr) {
     let maxSumNonAdj = 0;
 
-    /**
-     * start at arr[0] example
-     * arr[0] + a[0 + 2] + a[2 + 2]... a[n] total = sum(a[0] + ... + a[n])
-     * subsets: {a[0] + a[2]}, {a[0] + a[4]}... {a[0] + a[n]}
-     * */
+    // length of arr 
+    const n = arr.length
 
+    // edge case
+    if(n === 1) {
+        return Math.max(arr[0], 0);
+    };
+
+    let maxInt = arr[0] >= arr[1] ? arr[0]: arr[1];
+    let memo = [0, arr[0]];
+
+    for (let index = 2; index <= n; index++) {
+        const curVal = arr[index - 1];
+        const prevMax = memo[index - 2];
+        const currentMax = memo[index - 1];
+
+        if(curVal > maxInt) maxInt = curVal;
+
+        // compare the last value in memo against the sum of prev max, current max, and the current val in the case of negative numbers
+        memo.push(Math.max(prevMax + curVal, currentMax, curVal));
+    }
 
     // return the max sum of non-adjacent elements
-    return maxSumNonAdj;
+    return maxSumNonAdj = Math.max(0, memo.pop(), maxInt);
 }
 
 console.log(maxSubsetSum(arr));
