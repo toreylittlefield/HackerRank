992779dcdebd03751b73e0e48566ed1c5278d901
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-30 21:00:51 +0800

    Two Strings passing all tests

--- InterviewPrep/Dictionaries and Hashmaps/TwoStrings.js
+++ InterviewPrep/Dictionaries and Hashmaps/TwoStrings.js
@@ -14,8 +14,8 @@
 // input
 
 // Example 1: Expect Yes
-const s1 = `hello`
-const s2 = `world`
+// const s1 = `hello`
+// const s2 = `world`
 
 // Example 1: Expect No
 const s1 = `hi`
@@ -23,7 +23,33 @@ const s2 = `world`
 
 function twoStrings(s1, s2) {
     // Write your code here
+    let ans = 'NO'
 
+    // determine larger & smaller string and create map for larger string
+    let lgString = '';
+    let smString = '';
+    s1.length >= s2.length ? (lgString = s1, smString = s2) : (lgString = s2, smString = s1);
+    const lgMap = {};
+
+    // map over larger string
+    Array.from(lgString).forEach(letter => {
+        if(lgMap[letter]) {
+            lgMap[letter] ++;
+        } else {
+            lgMap[letter] = 1;
+        };
+    });
+
+    // if there is even 1 matching letter then return YES and end
+    Array.from(smString).forEach(letter => {
+        if(lgMap[letter]) {
+            ans = 'YES'
+            return;
+        };
+    })
+
+    // return Yes or No
+    return ans;
 }
 
 console.log(twoStrings(s1, s2));
