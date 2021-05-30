e05a1b59bf4b9240ee74868a0406fd4df499d968
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-31 07:58:58 +0800

    Sherlock & Anagrams problem setup: medium

---
+++ InterviewPrep/Dictionaries and Hashmaps/SherlockandAnagrams.js
@@ -0,0 +1,38 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/sherlock-and-anagrams/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+ * Type: Dictionaries and Hashmaps
+ * Difficulty: Medium
+ * Name: Sherlock and Anagrams
+ */
+
+/***
+ * Two strings are anagrams of each other if the letters of one string can be rearranged to form the other string. 
+ * Given a string, find the number of pairs of substrings of the string that are anagrams of each other.
+ */
+
+// Returns int: the number of unordered anagrammatic pairs of substrings in s
+
+// input
+
+// Example 1: Expect 4
+const s = `abba`
+
+// Example 2: Expect 0
+// const s = `abcd`
+
+// Example 3: Expect 3
+// const s = `ifailuhkqq`
+
+// Example 4: Expect 10
+// const s = `kkkk`
+
+// Example 5: Expect 5
+// const s = `cdcd`
+
+
+function sherlockAndAnagrams(s) {
+    // Write your code here
+
+}
+
+console.log(sherlockAndAnagrams(s));
