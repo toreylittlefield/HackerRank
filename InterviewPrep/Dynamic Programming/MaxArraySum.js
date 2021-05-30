799d437e5510b122d60636f27e38b06ae4a1e96c
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-30 08:24:10 +0800

    Max Array Sum problem setup

---
+++ InterviewPrep/Dynamic Programming/MaxArraySum.js
@@ -0,0 +1,23 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/max-array-sum/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dynamic-programming
+ * Type: Dynamic Programming
+ * Difficulty: Medium
+ * Name: Max Array Sum
+ */
+
+// Input int arr[n]: an array of integers
+
+// Output - int: the maximum subset sum
+
+// Example 1: Expect 13
+const arr = [3, 7, 4, 6, 5]
+
+// Example 2: Expect 11
+const arr = [2, 1, 5, 8, 4]
+
+function maxSubsetSum(arr) {
+
+
+}
+
+console.log(maxSubsetSum(arr));
