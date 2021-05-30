fa4e1d56ff6d1931282dd1452b4e57a1ba897ff6
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-30 08:48:22 +0800

    add extra comments

--- InterviewPrep/Dynamic Programming/MaxArraySum.js
+++ InterviewPrep/Dynamic Programming/MaxArraySum.js
@@ -8,7 +8,9 @@
 /**
  * Description:
  * Given an array of integers, find the subset of non-adjacent elements with the maximum sum.
- * Calculate the sum of that subset. It is possible that the maximum sum is 0, the case when all elements are negative.
+ * Calculate the sum of that subset. 
+ * It is possible that the maximum sum is 0, the case when all elements are negative.
+ *  Note that any individual element is a subset as well.
  */
 
 // Input int arr[n]: an array of integers
@@ -19,16 +21,23 @@
 const arr = [3, 7, 4, 6, 5]
 
 // Example 2: Expect 11
-const arr = [2, 1, 5, 8, 4]
+// const arr = [2, 1, 5, 8, 4]
 
 // Example 3: Expect 0 ** Special Negative Case
-const arr = [-2,-3,-1];
+// const arr = [-2,-3,-1];
 
 function maxSubsetSum(arr) {
     let maxSumNonAdj = 0;
 
+    /**
+     * start at arr[0] example
+     * arr[0] + a[0 + 2] + a[2 + 2]... a[n] total = sum(a[0] + ... + a[n])
+     * subsets: {a[0] + a[2]}, {a[0] + a[4]}... {a[0] + a[n]}
+     * */
+
+
     // return the max sum of non-adjacent elements
-    return maxSubsetSum;
+    return maxSumNonAdj;
 }
 
 console.log(maxSubsetSum(arr));
