75fbcc620802a5779814a8f96420b49d6e562194
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-30 08:28:27 +0800

    add 3rd case when negative example

--- InterviewPrep/Dynamic Programming/MaxArraySum.js
+++ InterviewPrep/Dynamic Programming/MaxArraySum.js
@@ -8,7 +8,7 @@
 /**
  * Description:
  * Given an array of integers, find the subset of non-adjacent elements with the maximum sum.
- * Calculate the sum of that subset. It is possible that the maximum sum is , the case when all elements are negative.
+ * Calculate the sum of that subset. It is possible that the maximum sum is 0, the case when all elements are negative.
  */
 
 // Input int arr[n]: an array of integers
@@ -21,9 +21,14 @@ const arr = [3, 7, 4, 6, 5]
 // Example 2: Expect 11
 const arr = [2, 1, 5, 8, 4]
 
+// Example 3: Expect 0 ** Special Negative Case
+const arr = [-2,-3,-1];
+
 function maxSubsetSum(arr) {
+    let maxSumNonAdj = 0;
 
-
+    // return the max sum of non-adjacent elements
+    return maxSubsetSum;
 }
 
 console.log(maxSubsetSum(arr));
