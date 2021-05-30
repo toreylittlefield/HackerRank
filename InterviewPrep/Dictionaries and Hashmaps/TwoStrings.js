d1eb4bb00924066b80b044cde121be7e1afa74b2
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-05-30 20:47:51 +0800

    Two Strings problem setup

---
+++ InterviewPrep/Dictionaries and Hashmaps/TwoStrings.js
@@ -0,0 +1,29 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/two-strings/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+ * Type: Dictionaries and Hashmaps
+ * Difficulty: Easy
+ * Name: Two Strings
+ */
+
+/***
+ * Given two strings, determine if they share a common substring. A substring may be as small as one character.
+ */
+
+// Returns string: 'Yes' or 'No' for each pair of strings
+
+// input
+
+// Example 1: Expect Yes
+const s1 = `hello`
+const s2 = `world`
+
+// Example 1: Expect No
+const s1 = `hi`
+const s2 = `world`
+
+function twoStrings(s1, s2) {
+    // Write your code here
+
+}
+
+console.log(twoStrings(s1, s2));
