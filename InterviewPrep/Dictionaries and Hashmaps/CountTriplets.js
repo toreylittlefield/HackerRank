cab857f46178c5b00677712b75519f3abb9a7ae1
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-01 16:31:07 +0800

    passing 12/13 tests. Solution fails for unsorted

--- InterviewPrep/Dictionaries and Hashmaps/CountTriplets.js
+++ InterviewPrep/Dictionaries and Hashmaps/CountTriplets.js
@@ -15,8 +15,8 @@
 // input
 
 // Example 1: Expect 2
-const arr = [1, 4, 16, 64]
-const r = 4
+// const arr = [1, 4, 16, 64]
+// const r = 4
 
 // Example 2: Expect 6
 // const arr = [1, 3, 9, 9, 27, 81]
@@ -34,38 +34,65 @@ const r = 4
 // const arr = Array(100).fill(1);
 // const r = 1;
 
+// Example 6: Expect 0
+const arr = [125, 5, 25, 5, 1]
+const r = 5
+
+// Example 7: Expect 4 {[0,1,2],[0,1,3],[0,2,3],[1,2,3]}
+// const arr = [1,1,1,1]
+// const r = 1;
+
 
 function countTriplets(arr, r) {
 
     // count # triplets found
     let countTrip = 0;
 
-    const arrLen = arr.length
+    const arrLen = arr.length;
 
     // edge case: no possible triplets
     if(arrLen < 3) return countTrip;
 
     // use a map
+    const numMap = {}
+    const tripletsMap = {};
 
-    
-    arr.forEach((num, idx) => {
-        for (let jdx = idx + 1; jdx < arrLen; jdx++) {
-            for (let kdx = jdx + 1; kdx < arrLen; kdx++) {
-                // console.log({idx, jdx, kdx})
-                const isValid = checkValidTriplet([arr[idx], arr[jdx], arr[kdx]], r);
-                if(isValid) countTrip++;
-            }
-        }
-    })
+    for (let idx = 0; idx < arrLen; idx++) {
+        const curNum = arr[idx]
+        let nextPartnerNum = curNum * r;
+
+        // console.log({curNum, nextPartnerNum})
+
+        // skip numbers not in geometric series as they cannot form triplets
+        if(curNum % r <= 1 && curNum > 0) {
+
+            // add to total by number of triplets
+            if(tripletsMap[curNum] ) {
+                countTrip += tripletsMap[curNum];
+            };
+
+            if(numMap[curNum]) {
+                if(tripletsMap[nextPartnerNum]) {
+                    tripletsMap[nextPartnerNum] += numMap[curNum]
+                } else {
+                    tripletsMap[nextPartnerNum] = numMap[curNum];
+                };
+            };
+
+            // add or increment numMap
+            if(numMap[nextPartnerNum]) {
+                numMap[nextPartnerNum]++;
+            } else {
+                numMap[nextPartnerNum] = 1;
+            };
+
+        };
+    };
+
+    // console.log({tripletsMap, numMap})
 
     // return the number of triplets in arr following the geometric progression and i < j < k
     return countTrip;
-
-    // method to check geometric progression && if valid triplet
-    function checkValidTriplet([i, j, k], r) {
-        // console.log({i,j,k})
-        return i <= j && i * r === j && j <= k && j * r === k;
-    };
 }
 
 console.log(countTriplets(arr, r));
