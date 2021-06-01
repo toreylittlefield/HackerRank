a0e163438768ef4bef08b62c6e65a9304e5595db
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-01 18:53:53 +0800

    Frequency Queries problem setup

---
+++ InterviewPrep/Dictionaries and Hashmaps/FrequencyQueries.js
@@ -0,0 +1,26 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/frequency-queries/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+ * Type: Dictionaries and Hashmaps
+ * Difficulty: Medium
+ * Name: Frequency Queries
+ */
+
+/***
+ * - 1 x : Insert x in your data structure.
+ * - 2 y : Delete one occurence of y from your data structure, if present.
+ * - 3 z : Check if any integer is present whose frequency is exactly z. If yes, print 1 else 0.
+ */
+
+// Returns int []: the results of queries of type 3
+
+// input
+
+// Example 1: Expect [0, 1] which is NO, YES
+const queries = [[1, 6],[3, 2],[1, 10],[1, 10],[1, 6],[2,5],[3, 2]]
+
+function freqQuery(queries) {
+
+
+}
+
+console.log(freqQuery(queries));
