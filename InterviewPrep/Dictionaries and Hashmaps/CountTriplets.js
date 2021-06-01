d50cc6febef6062a25991bab2fa8d47c7ec77125
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-01 09:12:24 +0800

    Count Triplets problem setup

---
+++ InterviewPrep/Dictionaries and Hashmaps/CountTriplets.js
@@ -0,0 +1,34 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/count-triplets-1/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+ * Type: Dictionaries and Hashmaps
+ * Difficulty: Medium
+ * Name: Count Triplets
+ */
+
+/***
+ * You are given an array and you need to find number of tripets of indices (i,j,k)
+ * such that the elements at those indices are in geometric progression for a given common ratio r and i < j < k.
+ */
+
+// Returns int: the number of triplets
+
+// input
+
+// Example 1: Expect 2
+const arr = [1, 4, 16, 64]
+const r = 4
+
+// Example 2: Expect 6
+// const arr = [1, 3, 9, 9, 27, 81]
+// const r = 3
+
+// Example 3: Expect 4
+// const arr = [1, 5, 5, 25, 125]
+// const r = 5
+
+
+function countTriplets(arr, r) {
+
+}
+
+console.log(countTriplets(arr, r));
