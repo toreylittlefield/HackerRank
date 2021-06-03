41466ed9d2b077e800f1a0c3d4224e3e7d6f8af9
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-03 19:56:31 +0800

    rm eval, add fn Calc math: need Order of Ops

--- 10 Days of JavaScript/Day 9/src/js/binaryCalculator.js
+++ 10 Days of JavaScript/Day 9/src/js/binaryCalculator.js
@@ -1,7 +1,7 @@
 function onButton(e) {
-    var btn = e.target || e.srcElement;
-    var action = document.getElementById(btn.id).innerHTML;
-    var res = document.getElementById('res');
+    let btn = e.target || e.srcElement;
+    let action = document.getElementById(btn.id).innerText;
+    let res = document.getElementById('res');
     
     switch(action) {
         case '0':
@@ -10,27 +10,64 @@ function onButton(e) {
         case '-':
         case '*':
         case '/':
-            res.innerHTML += action;
+            res.innerText += action;
             break;
         case 'C':
-            res.innerHTML = '';
+            res.innerText = '';
             break;
         case '=':
-            var expr = res.innerHTML;
-            var nums = /(\d+)/g;
+            let expr = res.innerText;
+            let nums = /(\d+)/g;
             // Replace all base 2 nums with base 10 equivs
             expr = expr.replace(nums, function(match) {
                 return parseInt(match, 2);
             })
             // eval in base 10 and convert to base 2
-            res.innerHTML = eval(expr).toString(2);
+            res.innerText = Math.floor(Calc(expr)).toString(2);
             break;
         default:
             console.error('should not be executed');
             break;
     }
+
+    function Calc(string) {
+        let result = 0;
+        const tempNum = [];
+        const ops = [];
+        Array.from(string).forEach(el => {
+            const isNumBool = isNaN(el) ? false : true;
+            if(isNumBool && !ops.length) {
+                tempNum.push(parseInt(el));
+            } else if(isNumBool && ops.length) {
+                el = parseInt(el);
+                const firstNum = tempNum.pop();
+                const operator = ops.pop();
+                switch(operator) {
+                    case '+': {
+                        result += (firstNum + el)
+                        break;
+                    };
+                    case '-': {
+                        result += (firstNum - el)
+                        break;
+                    };
+                    case '*': {
+                        result += (firstNum * el)
+                        break;
+                    };
+                    case '/': {
+                        result += (firstNum / el)
+                        break;
+                    };
+                };
+            } else {
+                ops.push(el);
+            };
+        });
+        return result;
+    };
 }
-var buttons = document.getElementsByTagName('button');
+let buttons = document.getElementsByTagName('button');
 for (let button of buttons) {
     button.onclick = onButton;
 }
