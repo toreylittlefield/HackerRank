cb46b7032598e8e8f1cec154d5388469bed4308b
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-04 14:29:19 +0800

    Sequence Equattion practice, easy. Passes tests.

---
+++ Practice/Algorithms/Implementation/SequenceEquation.js
@@ -0,0 +1,26 @@
+/**
+ * Link: https://www.hackerrank.com/challenges/permutation-equation/problem
+ * Type: Algorithms: Implementation
+ * Difficulty: Easy
+ * Name: Sequence Equation
+ */
+
+// Returns int[n]: the values of y for all x in the arithmetic sequence 1 to n
+
+// input p[n]: an array of intergers
+
+// Example 1: Expect {2, 3, 1}
+// const p = [2, 3, 1]
+
+// Example 2: Expect {1, 3, 5, 4, 2}
+const p = [4, 3, 5, 1, 2]
+
+function permutationEquation(p) {
+    // Write your code here
+    return p.reduce((acc, cur, idx) => {
+      acc[p[cur - 1] - 1] = idx + 1;
+      return acc;
+    } , []);
+};
+
+console.log(permutationEquation(p));
