64249e9fd115d71fea62825634ca4aeaf6960cfd
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-04 16:16:35 +0800

    Quartiles passes all tests

--- 10 Days of Statistics/Day 1/Quartiles.js
+++ 10 Days of Statistics/Day 1/Quartiles.js
@@ -13,14 +13,47 @@
  */
 
 
-// Example 1: Expect 6 12 16 which is in order of medians: left-half, median total, right-half
-const arr = [3, 7, 8, 5, 12, 14, 21, 13,18]
+// Example 1: Expect [6, 12, 16] which is in order of medians: left-half, median total, right-half
+// const arr = [3, 7, 8, 5, 12, 14, 21, 13,18]
 
-// Example 2: Expect {1, 3, 5, 4, 2}
+// Example 2: Expect [2, 4, 6]
+const arr = [1,3,5,7];
 
 function quartiles(arr) {
     // Write your code here
-    
+    const len = arr.length;
+
+    // our left & right quartile arrays
+    let left = [];
+    let right = [];
+
+    // median index position
+    let medianPos = Math.floor(len / 2);
+
+    let median = 0;
+
+    // sort the arr
+    arr.sort((a,b) => a - b);
+
+    // calculate left median
+    left = Math.floor(getMedian(arr.slice(0, medianPos)));
+
+    // calc right median if even or odd
+    right = len % 2 === 0 ? getMedian(arr.slice(medianPos)) : getMedian(arr.slice(medianPos + 1));
+
+    // calc the median of arr
+    median = getMedian(arr)
+
+
+    return [left, median, right];
+
+    // generic fn to calc median of an arr if even or odd length
+    function getMedian(qArr) {
+        let len = qArr.length;
+        let medianPos = Math.floor(len / 2);
+
+        return len % 2 === 0 ? ((qArr[medianPos - 1] + qArr[medianPos ]) / 2) : qArr[medianPos];
+    };
 }
 
 console.log(quartiles(arr));
