5127ef507f4faf611f98fd635c713bff69a5dfcb
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-04 14:48:27 +0800

    Quartiles problem setup

---
+++ 10 Days of Statistics/Day 1/Quartiles.js
@@ -0,0 +1,26 @@
+/**
+ * Link: https://www.hackerrank.com/challenges/s10-quartiles/problem
+ * Type: 10 Days of Statistics: Day 1
+ * Difficulty: Easy
+ * Name: Quartiles
+ */
+
+// Returns int[3]: the medians of the left half of arr, arr in total, and the right half of arr.
+
+/**
+ * Input: The first line contains an integer, n, the number of elements in arr.
+ * The second line contains n space-separated integers, each an arr[i].
+ */
+
+
+// Example 1: Expect 6 12 16 which is in order of medians: left-half, median total, right-half
+const arr = [3, 7, 8, 5, 12, 14, 21, 13,18]
+
+// Example 2: Expect {1, 3, 5, 4, 2}
+
+function quartiles(arr) {
+    // Write your code here
+    
+}
+
+console.log(quartiles(arr));
