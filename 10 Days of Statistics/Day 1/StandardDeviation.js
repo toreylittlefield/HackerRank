db273eefdfec1bb7e1b6eda851a466a08d85bc9d
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-04 16:48:01 +0800

    Standard Deviation problem setup

---
+++ 10 Days of Statistics/Day 1/StandardDeviation.js
@@ -0,0 +1,23 @@
+/**
+ * Link: https://www.hackerrank.com/challenges/s10-standard-deviation/problem?h_r=next-challenge&h_v=zen
+ * Type: 10 Days of Statistics: Day 1
+ * Difficulty: Easy
+ * Name: Standard Deviation
+ */
+
+//  Prints - float: the standard deviation to 1 place after the decimal
+
+// stdDev has the following parameters: - int arr[n]: an array of integers
+
+// Example 1: Expect 1.7
+const arr = [2, 5, 2, 7, 4]
+
+// Example 2: Expect 14.1
+// const arr =[10, 40, 30, 50, 20]
+
+function stdDev(arr) {
+    // Print your answers to 1 decimal place within this function
+
+}
+
+console.log(stdDev(arr))
