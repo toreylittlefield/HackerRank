ca6de7b6eb89f4957722e3ee8d0144b4eb5af7a4
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-04 17:03:08 +0800

    Standard Deviation passing all tests

--- 10 Days of Statistics/Day 1/StandardDeviation.js
+++ 10 Days of Statistics/Day 1/StandardDeviation.js
@@ -9,15 +9,25 @@
 
 // stdDev has the following parameters: - int arr[n]: an array of integers
 
-// Example 1: Expect 1.7
-const arr = [2, 5, 2, 7, 4]
+// Example 1: Expect 1.9
+// const arr = [2, 5, 2, 7, 4]
 
 // Example 2: Expect 14.1
-// const arr =[10, 40, 30, 50, 20]
+const arr = [10, 40, 30, 50, 20]
 
 function stdDev(arr) {
     // Print your answers to 1 decimal place within this function
+    let size = arr.length;
+    let mean = arr.reduce((a, b) => a + b)/size;
 
+    let sqDistance = arr.reduce((a , b) => {
+        return a + ((b - mean)**2);
+    }, 0);
+
+    
+    let stDev = Math.sqrt((sqDistance/size)).toFixed(1);
+    console.log(stDev);
+    return stDev;
 }
 
 console.log(stdDev(arr))
