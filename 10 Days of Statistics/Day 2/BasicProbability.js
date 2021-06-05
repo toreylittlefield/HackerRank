b9f81abbe2237ca7e37ec032255c886828db3582
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-05 20:27:59 +0800

    Basic Probability problem setup 10 Days Stats

---
+++ 10 Days of Statistics/Day 2/BasicProbability.js
@@ -0,0 +1,18 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/s10-mcq-1/problem
+ * Type: 10 Days of Statistics
+ * Difficulty: --
+ * Name: Day 2: Basic Probability
+ */
+
+// In a single toss of 2 fair (evenly-weighted) six-sided dice, find the probability that their sum will be at most 9.
+
+// Example 1: Expect 
+const dice = [6,6];
+const k = 9;
+
+function calcProb(dice, k) {
+
+}
+
+console.log(calcProb(dice, k))
