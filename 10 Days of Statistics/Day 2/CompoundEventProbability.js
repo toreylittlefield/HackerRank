0ba31d0cc878571eab607c6b85cf22e502bc9620
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-06 07:04:07 +0800

    Compound Event Probability problem setup

---
+++ 10 Days of Statistics/Day 2/CompoundEventProbability.js
@@ -0,0 +1,27 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/s10-mcq-3/problem
+ * Type: 10 Days of Statistics
+ * Difficulty: --
+ * Name: Day 2: Compound Event Probability
+ */
+
+// There are 3 urns labeled X, Y, and Z.
+
+
+// Urn X contains 4 red balls and 3 black balls.
+// Urn Y contains 5 red balls and 4 black balls.
+// Urn Z contains 4 red balls and 4 black balls.
+
+// One ball is drawn from each of the 3 urns. What is the probability that, of the 3 balls drawn, 2 are red and 1 is black?
+
+// Example 1: Expect ~33.33
+const X = ['red', 'red', 'red', 'red', 'black','black','black']
+const Y = ['red', 'red', 'red', 'red', 'red', 'black', 'black','black','black']
+const Z = ['red', 'red', 'red', 'red', 'black', 'black','black','black']
+const draw = ['red', 'red', 'black']
+
+function solveProb(X,Y,Z,draw) {
+
+}
+
+console.log(solveProb(X,Y,Z,draw))
