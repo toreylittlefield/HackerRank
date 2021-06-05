ceb17eab7cc65ae51b68a4c720677bccb3964f74
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-05 22:00:09 +0800

    clean up basic probability

--- Practice/Algorithms/Implementation/FormingAMagicSquare.js
+++ Practice/Algorithms/Implementation/FormingAMagicSquare.js
@@ -13,11 +13,38 @@
 // const s = [[5, 3, 4], [1, 5, 8], [6, 4, 2]]
 
 // Example 2: Expect 1
-const s = [[4, 9, 2],[3, 5, 7],[8, 1, 5]]
+// const s = [[4, 9, 2],[3, 5, 7],[8, 1, 5]]
+
+// Example 3: Expect 4
+// const s = [[4, 8, 2],[4, 5, 7],[6, 1, 6]];
+
+// Example 4: Expect 14
+const s = [[4, 5, 8],[2, 4, 1],[1, 9, 7]]
+        // [4, 5, 8] = 17 - 15 = 2
+        // [2,4,1] = 7 - 15 = 8
+        // [1,9,7] = 17 - 15 = 2 ---> 12
+// x = 4
+// x + 1 = [5 - 4] = 1
+// x - 1 = [3 - 7] = 4
+// x + 3 = [7 - 1] = 6
+// x - 3 = [1 - 8] = 7
 
 function formingMagicSquare(s) {
     // Write your code here
-
+    // const x = s[1][1];
+    // const xPlus1 = (x + 1) - s[0][0];
+    // const xMinus1 = (x - 1) - s[2][2];
+    // const xPlus3 = (x + 3) - s[2][0];
+    // const xMinus3 = (x - 3) - s[0][2];
+    // const array = [xPlus1, xMinus1, xPlus3, xMinus3]
+    // console.log(array)
+    let totalCostMoves = 0;
+    s.forEach( col => {
+        const sumCol = col.reduce((acc, num) =>  acc + num);
+        console.log(sumCol)
+        totalCostMoves += Math.abs(15 - sumCol);
+    })
+    return totalCostMoves
 };
 
 console.log(formingMagicSquare(s));
