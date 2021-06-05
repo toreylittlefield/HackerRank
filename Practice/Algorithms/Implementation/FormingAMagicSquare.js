845929301f61fb4770b17a6ccf3e1e056d023141
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-05 16:46:03 +0800

    Forming Magic Square practice medium difficulty

---
+++ Practice/Algorithms/Implementation/FormingAMagicSquare.js
@@ -0,0 +1,23 @@
+/**
+ * Link: https://www.hackerrank.com/challenges/magic-square-forming/problem
+ * Type: Algorithms: Implementation
+ * Difficulty: Medium
+ * Name: Forming a Magic Square
+ */
+
+// Returns int: the minimal total cost of converting the input square to a magic square
+
+// input int s[3][3]: a 3 x 3  array of integers
+
+// Example 1: Expect 7 
+// const s = [[5, 3, 4], [1, 5, 8], [6, 4, 2]]
+
+// Example 2: Expect 1
+const s = [[4, 9, 2],[3, 5, 7],[8, 1, 5]]
+
+function formingMagicSquare(s) {
+    // Write your code here
+
+};
+
+console.log(formingMagicSquare(s));
