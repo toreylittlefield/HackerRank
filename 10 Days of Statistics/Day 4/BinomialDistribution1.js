93f75d5876233bee786f19a0bb296ac4e3145843
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-08 22:18:30 +0800

    Binomial Distribution 1 passing all test

--- 10 Days of Statistics/Day 4/BinomialDistribution1.js
+++ 10 Days of Statistics/Day 4/BinomialDistribution1.js
@@ -25,11 +25,48 @@
  * Print a single line denoting the answer, rounded to a scale of 3 decimal places (i.e., 1.234 format).
  */
 
+// Binomial Distribution --> bdis(x,n,p) = ((n!)/(x!(n-x)!) * p^x * q^(n-x)
+
 // Example 1: Expect 0.696
 const input = `1.09 1`
 
  function processData(input) {
     //Enter your code here
-} 
+    const [maleR, femaleR] = input.split(' ').map(Number);
+    const probability = maleR / (maleR + femaleR);
+    const familySize = 6;
+    const numberMales = 3;
+
+    console.log(cumulativeProbability(numberMales, familySize, probability, cumProb = 0).toFixed(3))
+}
+
+function cumulativeProbability (x, n, p, cp = 0) {
+    for (let index = x; index <= n; index++) {
+        cp += binomialDistribution(index, n, p)
+    }
+    return cp;
+}
+
+function binomialDistribution (x, n, p) {
+    return combination(n, x) * (p ** x) * ((1 - p) ** (n - x))
+}
+
+function combination (n, x) {
+    return factorial(n) / (factorial(x) * factorial(n - x))
+}
+
+// Fibonacci not using recursion just a while loop, which is O(n) time and constant O(1) space complexity
+// it is also much faster than recurison for larger inputs because there is no huge number of calls added to the call stack
+const factorial = (n) => {
+    if(n < 2) return 1;
+    let factorialSum = 1;
+
+    while(n) {
+        factorialSum *= n;
+        n--;
+    };
+
+    return factorialSum;
+};
 
 console.log(processData(input))
