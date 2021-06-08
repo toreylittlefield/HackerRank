9ca86af163e5b4a30fc92c63587d056710b01c04
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-08 10:23:24 +0800

    Day 4 Statistics Binomial Distribution 1 setup

---
+++ 10 Days of Statistics/Day 4/BinomialDistribution1.js
@@ -0,0 +1,35 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/s10-binomial-distribution-1/problem
+ * Type: 10 Days of Statistics
+ * Difficulty: Easy
+ * Name: Day 4: Binomial Distribution I
+ */
+
+/**
+ * See Tutorial for Information: https://www.hackerrank.com/challenges/s10-binomial-distribution-1/tutorial
+ * Task:
+ * The ratio of boys to girls for babies born in Russia is 1.09:1.
+ * If there is 1 child born per birth, what proportion of Russian families with exactly 6
+ * children will have at least 3 boys?
+ * 
+ * Write a program to compute the answer using the above parameters. T
+ * hen print your result, rounded to a scale of 3 decimal places (i.e., 1.234 format).
+ * 
+ * Input Format
+ * 
+ * A single line containing the following values:
+ * 
+ * 1.09 1
+ * 
+ * Output Format
+ * Print a single line denoting the answer, rounded to a scale of 3 decimal places (i.e., 1.234 format).
+ */
+
+// Example 1: Expect 0.696
+const input = `1.09 1`
+
+ function processData(input) {
+    //Enter your code here
+} 
+
+console.log(processData(input))
