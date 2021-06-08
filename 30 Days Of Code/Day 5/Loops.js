83411802afd7878d170bf617731c019eccd1ad7f
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-08 09:11:40 +0800

    Day 5 of 30 Days Challenge: Loops Problem Setup

---
+++ 30 Days Of Code/Day 5/Loops.js
@@ -0,0 +1,37 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/30-loops/problem
+ * Type: 30 Days Of Code
+ * Difficulty: Easy
+ * Name: Day 5: Loops
+ */
+
+/**
+ * Objective
+In this challenge, we will use loops to do some math. Check out the Tutorial tab to learn more.
+
+Task
+Given an integer, n , print its first 10 multiples. 
+Each multiple n x i (where 1<=i<=10 ) should be printed on a new line in the form: n x i = result.
+
+Example n = 3
+
+The printout should look like this:
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+3 x 5 = 15
+3 x 6 = 18
+3 x 7 = 21
+3 x 8 = 24
+3 x 9 = 27
+3 x 10 = 30
+ */
+
+const n = 3;
+
+function first10multi(n) {
+    
+}
+
+console.log(first10multi(n))
