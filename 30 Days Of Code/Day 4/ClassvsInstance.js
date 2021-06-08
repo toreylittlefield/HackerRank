fc442ad8681053afaa49ee6940499b42e664c1de
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-08 08:52:42 +0800

    Class v. Instance passing all tests

--- 30 Days Of Code/Day 4/ClassvsInstance.js
+++ 30 Days Of Code/Day 4/ClassvsInstance.js
@@ -48,13 +48,26 @@ const initialAge = -1;
 
 
  function Person(initialAge){
+    this.age = initialAge;
+    if(this.age < 0) {
+        this.age = 0;
+        console.log("Age is not valid, setting age to 0.")
+    };
     // Add some more code to run some checks on initialAge
   this.amIOld=function(){
    // Do some computations in here and print out the correct statement to the console
+   if(this.age < 13) {
+       console.log("You are young.")
+   } else if(this.age < 18) {
+       console.log('You are a teenager.')
+   } else {
+       console.log('You are old.')
+   }
 
   };
    this.yearPasses=function(){
           // Increment the age of the person in here
+          this.age += 1;
    };
 }
 
