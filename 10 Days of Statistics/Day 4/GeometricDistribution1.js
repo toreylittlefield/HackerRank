56162823293dd7deb5ad1f2d6845b5624f07f785
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-09 21:27:35 -0400

    Geometric Distribution 1 passing all tests

--- 10 Days of Statistics/Day 4/GeometricDistribution1.js
+++ 10 Days of Statistics/Day 4/GeometricDistribution1.js
@@ -25,11 +25,26 @@ Output Format
 Print a single line denoting the answer, rounded to a scale of 3 decimal places.
  */
 
-const input = `1 3
-    5`
+// Binomial Distribution --> bdis(x,n,p) = ((n!)/(x!(n-x)!) * p^x * q^(n-x)
+// Negative Binomial Distribution --> negbdis*(x,n,p) = ((n - 1!)/((x - 1)!((n-1)-(x-1))!) * p^x * q^(n-x)
+
+// Expect 0.066
+const input = `1 3 
+5`
 
 function processData(input) {
     //Enter your code here
+    const [numDem, trials] = input.split('\n');
+    const [numerator, demoninator] = numDem.split(' ');
+    const probFail = numerator/demoninator;
+    const probSuccess = 1 - probFail;
+
+    // console.log({probFail, probSuccess, numerator,denominator, trials})
+    let results = (probFail**(1) * probSuccess**(trials - 1)).toFixed(3);
+
+    console.log(results);
+    return results
+
 } 
 
 console.log(processData(input))
