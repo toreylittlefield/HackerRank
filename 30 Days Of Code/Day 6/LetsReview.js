937363c1bcbf2ff56d81e9f02dea44d3aaa9b95a
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-09 20:23:09 -0400

    30 Days of Code Day 6: Let's Review Problem Setup

---
+++ 30 Days Of Code/Day 6/LetsReview.js
@@ -0,0 +1,42 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/30-review-loop/problem
+ * Type: 30 Days Of Code
+ * Difficulty: Easy
+ * Name: Day 6: Let's Review
+ */
+
+
+/**
+Task:
+Given a string, S, of length N that is indexed from 0 to N-1, print its even-indexed and odd-indexed characters 
+as 2 space-separated strings on a single line (see the Sample below for more detail).
+
+Note: 0 is considered to be an even index.
+
+Example:
+s = adbecf
+
+Print abc def
+
+Input Format
+
+The first line contains an integer, T (the number of test cases).
+Each line i of the T subsequent lines contain a string, S.
+
+Sample Input:
+2
+Hacker
+Rank
+
+Sample Output:
+Hce akr
+Rn ak
+ */
+
+const input = `Hacker`
+
+function processData(input) {
+    //Enter your code here
+} 
+
+console.log(processData(input))
