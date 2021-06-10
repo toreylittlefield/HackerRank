b7dbb6756a752c21c31c6053686e506a5a11ea7d
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-09 20:48:47 -0400

    10 Days Stats - Geo Distribution 1 Problem Setup

---
+++ 10 Days of Statistics/Day 4/GeometricDistribution1.js
@@ -0,0 +1,35 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/s10-geometric-distribution-1/problem
+ * Type: 10 Days of Statistics
+ * Difficulty: Easy
+ * Name: Day 4: Geometric Distribution I
+ */
+
+// See Tutorial : https://www.hackerrank.com/challenges/s10-geometric-distribution-1/tutorial
+
+/**
+Task:
+The probability that a machine produces a defective product is 1/3.
+What is the probability that the 1st defect occurs the 5th item produced?
+
+Input Format:
+The first line contains the respective space-separated numerator and denominator for the probability of a defect, 
+and the second line contains the inspection we want the probability of being the first defect for:
+    1 3
+    5
+
+If you do not wish to read this information from stdin, you can hard-code it into your program.
+
+Output Format
+
+Print a single line denoting the answer, rounded to a scale of 3 decimal places.
+ */
+
+const input = `1 3
+    5`
+
+function processData(input) {
+    //Enter your code here
+} 
+
+console.log(processData(input))
