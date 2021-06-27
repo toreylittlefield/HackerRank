48defddac033121d48d66e670fb2c0cdc207727e
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-06-27 15:36:00 +0800

    30 Days Code: Day 11 2D Arrays solution

---
+++ 30 Days Of Code/Day 11/2DArray.js
@@ -0,0 +1,38 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/30-binary-numbers/problem
+ * Type: 30 Days Of Code
+ * Difficulty: Easy
+ * Name: Day 10: Binary Numbers
+ */
+
+
+function main() {
+
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    };
+
+    const len = arr.length;
+    let max = 0;
+    let results = []
+    for(let col = 0; col < len/2; col++) {
+        const [leftCol, rightCol] = [[col], [col + 1]];
+        for(let r = 0; col < len/2; r++) {
+            
+            const [rowTop, row, rowBottom] = [[r], [r + 1], [r + 2]];
+            // [0,0], [1,0], [2,0]
+            const top = arr[leftCol][rowTop] + arr[col][rowTop] + arr[rightCol][rowTop];
+            // [1,1]
+            const middle = arr[col][row];
+            // [0,2], [1,2], [2,2]
+            const bottom = arr[leftCol][rowBottom] + arr[col][rowBottom] + arr[rightCol][rowBottom];
+            const sum = top + middle + bottom;
+            results.push(sum)
+            max = Math.max(sum, max)
+        };
+    };
+    console.log(max)
+    return max;
+}
