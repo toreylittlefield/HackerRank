964625fe6883e740f1b6ec0404440492eae91d54
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-11-26 06:39:39 +0800

    Recusrion: davis' staircase setup

---
+++ InterviewPrep/Recursion/davis-staircase.ts
@@ -0,0 +1,24 @@
+/*
+ * Link: https://www.hackerrank.com/challenges/ctci-recursive-staircase/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=recursion-backtracking
+ * Type: Recursion and Backtracking
+ * Difficulty: Medium
+ * Name: Recursion: Davis' Staircase
+ */
+
+/* 
+Sample Input
+STDIN   Function
+-----   --------
+3       s = 3 (number of staircases)
+1       first staircase n = 1
+3       second n = 3
+7       third n = 7
+
+Returns
+int: the number of ways Davis can climb the staircase, modulo 10000000007
+
+Sample Output
+1
+4
+44
+*/
